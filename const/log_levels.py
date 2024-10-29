c4fdd02c44f52a81c98424a28670d9f3dd48af9c
Вахитова Гульназ <gulnaz.vakhitova.00@mail.ru>
2024-10-29 10:19:24 +0500

    fix(log_levels): Исправлено название класса

---
+++ const/log_levels.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Levels(Enum):
+    TRACE = "TRACE"
+    INFO = "INFO"
+    WARN = "WARN"
+    ERROR = "ERROR"
+    FATAL = "FATAL"
--- module/log_levels.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class LEVELS(Enum):
-    TRACE = 'TRACE'
-    INFO = 'INFO'
-    WARN = 'WARN'
-    ERROR = 'ERROR'
-    FATAL = 'FATAL'
